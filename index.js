6516d48a8582ed866814b564e8f750fffe90807b
Guilherme P Macedo <gpmmacedo@hotmail.com>
2026-05-31 18:43:30 -0400

    Add function to compare human and computer choice
    Forgot to commit for a while. Prepared a way to
    keep score and to compare and declare the winner.

--- index.js
+++ index.js
@@ -13,14 +13,37 @@ function getComputerChoice() {
 /* Prompt the user for their choice */
 
 function getUserChoice() {
-  let userChoice = prompt("Rock, paper, scissors? ", "Pick one");
+  let userChoice = prompt("Rock, paper, scissors? ", "Pick one").toLowerCase();
   return userChoice;
 }
 
 /* Compare the two choices */
+function isATie(humanChoice, computerChoice) {
+  humanChoice === computerChoice
+    ? alert("It's a tie!")
+    : compareChoices(humanChoice, computerChoice);
+}
+
+function compareChoices(humanChoice, computerChoice) {
+  if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    console.log(`You Win! ${humanChoice} beats ${computerChoice}`);
+    humanScore++;
+  }
+	else {
+		alert(`You lose. ${computerChoice} beats ${humanChoice}`);
+		computerScore++;
+	}
+}
+
 /* Keep score */
+let humanScore = 0;
+let computerScore = 0;
+
 /* Call functions */
 
 getUserChoice();
 getComputerChoice();
-
