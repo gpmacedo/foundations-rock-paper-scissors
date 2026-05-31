58311c1cd0ec67f9748154afaeb7ce4593614eb8
Guilherme P Macedo <gpmmacedo@hotmail.com>
2026-05-31 17:41:27 -0400

    Add computer choice function

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 /* Prompt the user for their choice */
 /* Randomize the computer choice */
+function getComputerChoice() {
+  let choiceNumber = Math.random() * 100;
+  if (choiceNumber < 34) {
+    choice = "rock";
+  } else if (choiceNumber < 67) {
+    choice = "paper";
+  } else {
+    choice = "scissors";
+  }
+  console.log(choiceNumber);
+	console.log(choice);
+}
+
+getComputerChoice();
 /* Compare the two choices */
 /* Keep score */
